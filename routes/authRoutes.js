26ce826f2861eb3836134073fb308e2efbe46591
shubkasar <shubhamk2409@gmail.com>
2024-08-18 17:50:29 -0400

    google auth working

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {register, login} = require('../controllers/authController');
 const authenticateToken = require('../middleware/authMiddleware');
+const passport = require('passport');
 
 const router = express.Router();
 
@@ -8,6 +9,22 @@ router.post('/register', register);
 router.post('/login', login);
 router.get('/protected', authenticateToken, (req, res) => {
     res.json({ message: "This is a protected route", user: req.user });
+});
+
+router.get('/google', passport.authenticate('google',{ scope: ['profile', 'email']}));
+
+router.get('/google/callback',
+    passport.authenticate('google', {failureRedirect: '/auth/login'}),
+    (req, res) => {
+        req.login(req.user, function(err){
+            if(err){
+                console.error("Session creation failed: ", err);
+                return res.redirect('/auth/login');
+            }
+        })
+        console.log('User authenticated: ', req.user);
+        console.log('Session: ', req.session);
+        res.redirect('/');
 });
 
 module.exports = router;
